bbe7eab6b20cd8ad8dcc9507472a293f39950a24
Anderson Nunes Santos <andersonnunes0300@gmail.com>
2026-04-02 16:27:34 -0300

    Implementando editar fornecedor

---
+++ backend/fornecedor_editar.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require_once '../conexao/conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST['id'];
+    $nome = $_POST['nome_empresa'];
+    $cnpj = $_POST['cnpj'];
+
+    $sql = "UPDATE fornecedores SET nome_empresa = ?, cnpj = ? WHERE id_fornecedor = ?";
+    $stmt = $conn->prepare($sql);
+    
+    $stmt->bind_param("ssi", $nome, $cnpj, $id);
+
+    if ($stmt->execute()) {
+        echo "<script>alert('Fornecedor atualizado!'); window.location.href='../frontend/xxxxxxx.php';</script>";
+    } else {
+        echo "Erro ao atualizar fornecedor: " . $conn->error;
+    }
+}
+?>
