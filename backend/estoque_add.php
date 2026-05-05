e9c1506da064d409c62fd715a24ae3ef06151ee4
Anderson Nunes Santos <andersonnunes0300@gmail.com>
2026-05-04 23:51:21 -0300

    Adicionar ao estoque

---
+++ backend/estoque_add.php
@@ -0,0 +1,32 @@
+<?php
+require_once '../conexao/conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+$id = $_POST['id_medicamento'];
+$nome = $_POST['nome'];
+$tipo = $_POST['tipo'];
+$tarja = $_POST['tarja'];
+$lote = $_POST['lote'];
+$validade = $_POST['validade'];
+$quantidade = $_POST['quantidade'];
+$preco = $_POST['preco'];
+$fornecedor = $_POST['id_fornecedor'];
+
+$stmt = $conn->prepare("
+INSERT INTO medicamentos
+(id_medicamento,nome,tipo,tarja,lote,validade,quantidade,preco,id_fornecedor)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+");
+
+$stmt->bind_param("ssssssidi",
+$id,$nome,$tipo,$tarja,$lote,$validade,$quantidade,$preco,$fornecedor
+);
+
+if ($stmt->execute()) {
+echo "<script>alert('Medicamento cadastrado!'); window.location.href='../frontend/xxxxx.php';</script>";
+}else{
+    echo "Erro ao Cadastrar o medicamento" . $conn->error;
+}
+}
+?>
