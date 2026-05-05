5d9ee2a3f8af630630e736cdddfc3a4654600c89
Anderson Nunes Santos <andersonnunes0300@gmail.com>
2026-05-04 23:39:31 -0300

    estoque update

---
+++ backend/estoque_update.php
@@ -0,0 +1,39 @@
+<?php
+require_once '../conexao/conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $id = $_POST['id_medicamento'];
+        $nome = $_POST['nome'];
+        $tipo = $_POST['tipo'];
+        $tarja = $_POST['tarja'];
+        $lote = $_POST['lote'];
+        $validade = $_POST['validade'];
+        $quantidade = $_POST['quantidade'];
+        $id_fornecedor = $_POST['id_fornecedor'];
+
+$stmt = $conn->prepare("
+UPDATE medicamentos
+SET nome=?, tipo=?, tarja=?, lote=?, validade=?, quantidade=?, preco=?, id_fornecedor=?
+WHERE id_medicamento=?
+");
+
+        $stmt->bind_param("sssssdiss", 
+            $nome, 
+            $tipo, 
+            $tarja, 
+            $lote, 
+            $validade, 
+            $quantidade, 
+            $preco, 
+            $id_fornecedor, 
+            $id
+        );
+
+if ($stmt->execute()) {
+echo "<script>alert('Medicamento atualizado!'); window.location.href='../frontend/xxxxx.php';</script>";
+}else{
+    echo "Erro ao atualizar!" . $conn->error;
+}
+}
+?>
