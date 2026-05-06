ddb71527e6090ee9ec8b957de8be0047d5114944
dscdiegodev <soaresdsc91@gmail.com>
2026-05-06 14:17:53 -0300

    Tela de Login criado com HTML

--- backend/login.php
+++ backend/login.php
@@ -13,15 +13,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($row = $resultado->fetch_assoc()) {
 
-        if ($senha === $row['senha']) { 
+        if ($senha === $row['senha']) {
             $_SESSION['id_usuario'] = $row['id_usuario'];
             $_SESSION['nome'] = $row['nome'];
-            
-            header("Location: ../frontend/xxxxxxxxxxx.php"); 
+
+            header("Location: ../frontend/xxxxxxxxxxx.php");
             exit;
         }
     }
-    
+
     echo "<script>alert('Usuário ou senha incorretos!'); window.location.href='../frontend/xxxxxxxx';</script>";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-Br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>IntegraFarma</title>
+</head>
+
+<body>
+    <div class="container-login">
+        <div class="caixa-login">
+            <!-- logo da farmácia  -->
+            <img src="../img/integrafarma.png" alt="Logo Integra Farma" class="logo-farmacia">
+
+            <form class="form-login" action="dashboard.php" method="POST">
+                <h2>Login Integra Farma</h2>
+
+                <!-- E-mail -->
+                <label for="inputEmail" class="form-label">Email:</label>
+                <input type="email" name="email" class="form-control" id="inputEmail" placeholder="nome@exemplo.com" required>
+
+                <!-- Senha -->
+                <label for="inputSenha" class="form-label">Senha:</label>
+                <input type="password" name="senha" class="form-control" id="inputSenha" placeholder="Digite sua senha" required>
+
+                <!-- Botão entrar -->
+                <button type="submit" class="btn-entrar">Entrar</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
+
+</html>
