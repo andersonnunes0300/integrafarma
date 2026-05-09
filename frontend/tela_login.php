f0e1436de1b8ffa2d6c3d63e3283523bf80743bf
dscdiegodev <soaresdsc91@gmail.com>
2026-05-09 13:02:30 -0300

    Código recebido, tela_login.php criada

---
+++ frontend/tela_login.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="pt-Br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>IntegraFarma</title>
+</head>
+
+<body>
+    <div class="container-login">
+        <div class="caixa-login">
+            <!-- logo da farmácia  -->
+            <img src="../img/integrafarma.png" alt="Logo Integra Farma" class="logo-farmacia">
+
+            <form class="form-login" action="dashboard.php" method="POST">
+                <h2>Login Integra Farma</h2>
+
+                <!-- E-mail -->
+                <label for="inputEmail" class="form-label">Email:</label>
+                <input type="email" name="email" class="form-control" id="inputEmail" placeholder="nome@exemplo.com" required>
+
+                <!-- Senha -->
+                <label for="inputSenha" class="form-label">Senha:</label>
+                <input type="password" name="senha" class="form-control" id="inputSenha" placeholder="Digite sua senha" required>
+
+                <!-- Botão entrar -->
+                <button type="submit" class="btn-entrar">Entrar</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
+
+</html>
