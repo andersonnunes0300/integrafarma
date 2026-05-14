914d3b2ea794f79e017a3c979fc6e60d5f7d6209
dscdiegodev <soaresdsc91@gmail.com>
2026-05-14 16:13:43 -0300

    Arquivo tela.php deletada (sem uso)

--- frontend/tela_login.php
+++ frontend/tela_login.php
@@ -1,3 +1,35 @@
+<?php
+session_start();
+
+require_once '../conexao/conexao.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha' LIMIT 1";
+    $resultado = $conn->query($sql);
+
+    if ($resultado && $resultado->num_rows > 0) {
+
+        $dados = $resultado->fetch_assoc();
+
+        $_SESSION['usuario_id'] = $dados['id_usuario'];
+        $_SESSION['usuario_login'] = $dados['login'];
+        $_SESSION['usuario_nivel'] = $dados['nivel'];
+
+        if ($_SESSION['usuario_nivel'] === 'admin') {
+            header("Location: tela_admin.php");
+        } else {
+            header("Location: tela_usuario.php");
+        }
+        exit;
+    } else {
+        echo "<script>alert('Login ou senha incorretos. Tente novamente.');</script>";
+    }
+}
+?>
+
 
 <!DOCTYPE html>
 <html lang="pt-Br">
@@ -25,14 +57,20 @@
                 <!-- Senha -->
                 <label for="inputSenha" class="form-label">Senha:</label>
                 <input type="password" name="senha" class="form-control" id="inputSenha" placeholder="Digite sua senha" required>
+                
+                <div class="nivel">
+                    <p class="texto">Nível de Acesso:</p>
+                    <select name="item-escolha" id="nivel-acesso">
+                        <option value="gerente">Funcionário</option>
+                        <option value="funcionario">Admin</option>
+                    </select>
+                </div>
 
                 <!-- Botão entrar -->
                 <button type="submit" class="btn-entrar">Entrar</button>
             </form>
 
-            <div class="links">
-            Não tem uma conta? <a href="usuario_cad.php">Cadastre-se</a><br>
-            </div>
+
         </div>
     </div>
 </body>
