8066d249c2523b7ffaea89b1ba841a95e1805163
dscdiegodev <soaresdsc91@gmail.com>
2026-05-16 13:05:45 -0300

    Ajuste para update de usuários

--- backend/usuario_editar.php
+++ backend/usuario_editar.php
@@ -1,23 +1,21 @@
 <?php
+session_start();
 require_once '../conexao/conexao.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     $id = $_POST['id_usuario'];
     $nome = $_POST['nome'];
     $login = $_POST['login'];
     $nivel = $_POST['nivel'];
 
+    $stmt = $conn->prepare("
+     UPDATE usuarios SET nome = ?, login = ?, nivel = ? WHERE id_usuario = ?");
 
-    $sql = "UPDATE usuarios SET nome = ?, login = ?, nivel = ? WHERE id_usuario = ?";
-    $stmt = $conn->prepare($sql);
-    
     $stmt->bind_param("sssi", $nome, $login, $nivel, $id);
 
     if ($stmt->execute()) {
-        echo "<script>alert('Usuário atualizado com sucesso!'); window.location.href='../frontend/xxxxxxx.php';</script>";
+        echo "<script>alert('Usuário atualizado com sucesso!'); window.location.href='../frontend/tela_admin.php';</script>";
     } else {
         echo "Erro ao atualizar o usuário: " . $conn->error;
     }
 }
-?>
