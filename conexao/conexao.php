32e62d4b81dd1bacda52080b577220167a235a03
dscdiegodev <soaresdsc91@gmail.com>
2026-05-18 14:46:08 -0300

    Função de registrar log craida

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -12,4 +12,20 @@ if($conn->connect_error){
 
 $conn->set_charset("utf8");
 
+// funação para registrar logs de atividades
+function registrarLog($conn, $id_usuario, $acao) {
+    $stmt = $conn->prepare("INSERT INTO logs_historico (id_usuario, acao) VALUES (?, ?)");
+    if (!$stmt) {
+        die("<br>Erro no prepare do banco:</br>" . $conn->error);
+    }
+
+    $stmt->bind_param("is", $id_usuario, $acao);
+
+    if (!$stmt->execute()) {
+        die("<br>Erro ao executar o comando do log:</br> " . $conn->error);
+    }
+
+    $stmt->close();
+}
+
 ?>
