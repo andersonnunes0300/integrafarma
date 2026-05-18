2e09e034f03279aad06fb07a45c2c738b414c79c
dscdiegodev <soaresdsc91@gmail.com>
2026-05-18 19:28:28 -0300

    Atualizando caminho e log de cadastro

--- backend/fornecedor_cadastrar.php
+++ backend/fornecedor_cadastrar.php
@@ -2,22 +2,22 @@
 require_once '../conexao/conexao.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST['nome_empresa'];
+    $nome_empresa = $_POST['nome_empresa'];
     $cnpj = $_POST['cnpj'];
 
     try {
         $stmt = $conn->prepare("INSERT INTO fornecedores (nome_empresa, cnpj) VALUES (?, ?)");
-        $stmt->bind_param("ss", $nome, $cnpj);
+        $stmt->bind_param("ss", $nome_empresa, $cnpj);
 
         if ($stmt->execute()) {
-            echo "<script>alert('Fornecedor cadastrado com sucesso!'); window.location.href='../frontend/xxxxx.php';</script>";
+            echo "<script>alert('Fornecedor cadastrado com sucesso!'); window.location.href='../frontend/listas_users_fornecedores.php';</script>";
         }
     } catch (mysqli_sql_exception $e) {
 
         if ($e->getCode() == 1062) {
-            echo "<script>alert('Erro: Este CNPJ já está cadastrado!'); window.location.href='../frontend/xxxxxx.php';</script>";
+            echo "<script>alert('Erro: Este CNPJ já está cadastrado!'); window.location.href='../frontend/listas_users_fornecedores.php';</script>";
         } else {
-            echo "<script>alert('Erro no banco: " . $e->getMessage() . "'); window.location.href='../frontend/xxxxxx.php';</script>";
+            echo "<script>alert('Erro no banco: " . $e->getMessage() . "'); window.location.href='../frontend/listas_users_fornecedores.php';</script>";
         }
     }
 }
