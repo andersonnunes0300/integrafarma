539cd289e41c98ffd739cfc6f85f16a431409d88
dscdiegodev <soaresdsc91@gmail.com>
2026-05-18 19:31:07 -0300

    Função excluir inserida no botão excluir

--- backend/fornecedor_delete.php
+++ backend/fornecedor_delete.php
@@ -9,10 +9,10 @@ $stmt->bind_param("i", $id);
 
 try {
     $stmt->execute();
-    header("Location: ../frontend/xxxxxxxxx.php?msg=sucesso");
+    header("Location: ../frontend/listas_users_fornecedores.php?msg=sucesso");
 } catch (Exception $e) {
 
-    echo "<script>alert('Erro: Este fornecedor possui medicamentos cadastrados e não pode ser removido.'); 
-          window.location.href='../frontend/xxxxxx.php';</script>";
+    echo "<script>alert('Erro: Este fornecedor possui medicamentos cadastrados e não pode ser removido.');
+          window.location.href='../frontend/listas_users_fornecedores.php';</script>";
 }
 ?>
