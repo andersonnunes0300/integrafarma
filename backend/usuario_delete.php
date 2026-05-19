4e71aad89c6817869111171ba01ba22c703e6f82
andersonnunes0300 <andersonnunes0300@gmail.com>
2026-05-19 14:12:15 -0300

    Ajustando a chamativa
    Estava com xxxx a chamativa, foi feito a correção.

--- backend/usuario_delete.php
+++ backend/usuario_delete.php
@@ -3,14 +3,14 @@ session_start();
 require_once '../conexao/conexao.php';
 
 if (!isset($_GET['id'])) {
-    header("Location: ../frontend/xxxxxxx.php");
+    header("Location: ../frontend/listas_users_fornecedores.php.php");
     exit;
 }
 
 $id = $_GET['id'];
 
 if ($id == $_SESSION['id_usuario']) {
-    echo "<script>alert('Atenção: Você não pode excluir seu próprio usuário!'); window.location.href='../frontend/xxxxxxxx.php';</script>";
+    echo "<script>alert('Atenção: Você não pode excluir seu próprio usuário!'); window.location.href='../frontend/listas_users_fornecedores.php';</script>";
     exit;
 }
 
@@ -19,7 +19,7 @@ $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $id);
 
 if ($stmt->execute()) {
-    echo "<script>alert('Usuário removido!'); window.location.href='../frontend/xxxxxxxx';</script>";
+    echo "<script>alert('Usuário removido!'); window.location.href='../frontend/listas_users_fornecedores.php';</script>";
 } else {
     echo "Erro ao excluir usuário: " . $conn->error;
 }
